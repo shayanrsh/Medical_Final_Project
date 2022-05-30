ff7d10bc98282bea45fab7f9436f7252f2772a3b
Shayan Rahimi <1shayanrsh@gmail.com>
2022-05-30 18:31:01 +0300

    Changing variable names and inverting the if

--- model_test.py
+++ model_test.py
@@ -2,6 +2,7 @@ import os
 import sys
 from glob import glob
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -16,31 +17,29 @@ from dataset_loader import load_dataset
 
 def model_test(net, batch_size=2):
     x_tensor, y_tensor, m_tensor = load_dataset(mode='test', resize=True, resize_shape=(256, 256))
-    num_samples = x_tensor.shape[0]
-    print("[+] ====== Start test... ======")
-    num_iters = int(np.ceil(num_samples / batch_size))
-    for ite in range(num_iters):
-        print("[*] predicting on the {}th batch".format(ite + 1))
-        if not ite == num_iters - 1:
-            start_id, end_id = ite * batch_size, (ite + 1) * batch_size
-            bat_img = torch.Tensor(x_tensor[start_id: end_id, :, :, :])
-            bat_label = torch.Tensor(y_tensor[start_id: end_id, 0: 1, :, :])
-            # bat_mask_2ch = torch.Tensor(m_tensor[start_id : end_id, :, :, :])
-            bat_mask = torch.Tensor(m_tensor[start_id: end_id, 0: 1, :, :])
+    sample_numbers = x_tensor.shape[0]
+    iteration_number = int(np.ceil(sample_numbers / batch_size))
+    for iteration in range(iteration_number):
+        print("{}th batch prediction".format(iteration + 1))
+        if iteration == iteration_number - 1:
+            start = iteration * batch_size
+            batch_image = torch.Tensor(x_tensor[start:, :, :, :])
+            batch_label = torch.Tensor(y_tensor[start:, 0: 1, :, :])
+            batch_mask = torch.Tensor(m_tensor[start:, 0: 1, :, :])
         else:
-            start_id = ite * batch_size
-            bat_img = torch.Tensor(x_tensor[start_id:, :, :, :])
-            bat_label = torch.Tensor(y_tensor[start_id:, 0: 1, :, :])
-            # bat_mask_2ch = torch.Tensor(m_tensor[start_id : end_id, :, :, :])
-            bat_mask = torch.Tensor(m_tensor[start_id:, 0: 1, :, :])
-        bat_pred = net(bat_img)
-        bat_pred_class = (bat_pred > 0.5).float() * bat_mask
-        eval_print_metrics(bat_label, bat_pred, bat_mask)
-        # plt.imshow(bat_pred[0,0,:,:].detach().numpy(), cmap='jet')#, vmin=0, vmax=1)
-        # plt.colorbar()
-        # plt.show()
-        # bat_pred_class = bat_pred.detach() * bat_mask
-        paste_and_save(bat_img, bat_label, bat_pred_class, batch_size, ite + 1)
+            start, end = iteration * batch_size, (iteration + 1) * batch_size
+            batch_image = torch.Tensor(x_tensor[start: end, :, :, :])
+            batch_label = torch.Tensor(y_tensor[start: end, 0: 1, :, :])
+            batch_mask = torch.Tensor(m_tensor[start: end, 0: 1, :, :])
+        batch_prediction = net(batch_image)
+        eval_print_metrics(batch_label, batch_prediction, batch_mask)
+
+        batch_prediction_class = batch_prediction.detach() * batch_mask
+        paste_and_save(batch_image, batch_label, batch_prediction_class, batch_size, iteration + 1)
+
+    plt.imshow(batch_prediction[0, 0, :, :].detach().numpy(), cmap='jet')
+    plt.colorbar()
+    plt.show()
 
     return
 
@@ -53,7 +52,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     selected_model = glob("storage/checkpoint/Unet_epoch*.model")[-1]
-    print("[*] Selected model for testing: {} ".format(selected_model))
+    print("testing: {} ".format(selected_model))
     unet_ins = Unet(img_ch=3, isDeconv=True, isBN=True)
     unet_ins.load_state_dict(torch.load(selected_model))
     unet_ins.to(device)
